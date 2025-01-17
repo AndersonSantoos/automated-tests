1ff79279ed21690f9136337183ccc317ae0f217c
Anderson Santos <andersonsantoos-10@outlook.com>
2025-01-16 21:53:32 -0300

    inserção do ID do nosso projeto no cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "6h7m16",
   e2e: {
+    //testIsolation: false
     setupNodeEvents(on, config) {
       return config;
     },
